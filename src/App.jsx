46a75b1f4c3128c56b9107258cc4c1c48ceae18d
FlorentLem <florentlempro@gmail.com>
2021-01-19 17:09:23 +0100

    initialisation application

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Router from './Router'
+import './App.css'
+import logo from './reactlogo.png';
+
+function App() {
+  return <Router />;
+}
+
+export default App;
