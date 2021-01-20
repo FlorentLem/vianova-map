6d7a333bcf9d630fb3a9851541d8927d21aee1ce
FlorentLem <florentlempro@gmail.com>
2021-01-20 02:12:00 +0100

    markers done

--- src/components/Elements/Map/Map.jsx
+++ src/components/Elements/Map/Map.jsx
@@ -7,6 +7,7 @@ import mapboxgl from 'mapbox-gl';
 
 // Import Geojson data to display
 import districtNY from './data/districtNewYork.geojson';
+import marker from './assets/markerStation.png';
 
 // Providing token to generate the map
 mapboxgl.accessToken =
@@ -26,6 +27,7 @@ class Map extends Component {
   // ComponentDidMount for generating the map when loading the page
   componentDidMount() {
     // Destructuring usefull items
+    const { stationList, setSelectedMarker } = this.props;
     const { lat, lng, zooom } = this.state;
     const { mapContainer } = this;
 
@@ -37,6 +39,18 @@ class Map extends Component {
       zoom: zooom,
       attributionControl: false,
     });
+
+    // Adding Geolocalisation and Navigation Controls to the map
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: false,
+        },
+        trackUserLocation: true,
+        fitBoundsOptions: { maxZoom: 8 },
+      })
+    );
+    map.addControl(new mapboxgl.NavigationControl());
 
     // Creating district layer with the geojson file as a source
     map.on('load', () => {
@@ -55,6 +69,58 @@ class Map extends Component {
           'fill-color': '#FFF',
           'fill-opacity': 0.8,
         },
+      });
+    });
+
+    const coord = stationList.map((el, index) => {
+      return {
+        type: 'Feature',
+        properties: {
+          id: index,
+          location: el.name,
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [el.lon, el.lat],
+        },
+      };
+    });
+
+    map.on('load', () => {
+      map.loadImage(marker, (error, image) => {
+        if (error) throw error;
+        map.addImage('custom-marker', image);
+        map.addSource('points', {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: coord,
+          },
+        });
+
+        map.addLayer({
+          id: 'points',
+          type: 'symbol',
+          minzoom: 12,
+          source: 'points',
+          layout: {
+            'icon-image': 'custom-marker',
+            'icon-size': 0.06,
+          },
+        });
+
+        map.on('click', 'points', (e) => {
+          const coordinates = e.features[0].geometry.coordinates.slice();
+          const place = e.features[0].properties.location;
+
+          new mapboxgl.Popup().setLngLat(coordinates).setHTML(place).addTo(map);
+          map.flyTo({
+            center: e.features[0].geometry.coordinates,
+            zoom: 17,
+            speed: 1,
+          });
+          setSelectedMarker(e.features[0].properties.id);
+        });
       });
     });
   }
